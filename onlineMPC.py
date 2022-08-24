35da53fb5225926634b59c5257c9e3714d55d921
Teng Guo <2274880117@qq.com>
2022-08-24 00:15:10 -0400

    online

---
+++ onlineMPC.py
@@ -0,0 +1,364 @@
+
+from common import *
+
+
+MAX_HORIZON=1000
+OUTVERTEX=(-1,-1)
+
+
+class Vertex(object):
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y    
+        self.visiting_agents=[]
+
+
+
+class onlineMCP(object):
+    def __init__(self,xmax,ymax):
+        self.xmax=xmax
+        self.ymax=ymax
+        self.location_id=dict()
+        self.future_location_id=dict()
+        self.occupied_port=set()
+        self.unused_agents=[]
+        self.used_agents=[]
+        self.parking_agents=[]
+        self.parked_agents=[]
+        self.retrieving_agents=[]
+        self.parking_probability=0.6
+        self.retrieving_probability=0.1
+        self.agents=[]
+        self.vertices=dict()
+        self.clock=0
+
+    def fill_paths(self,makespan=-1):
+        # makespan=0
+        if makespan<0:
+            for agent in self.agents:
+                makespan=max(len(agent.path),makespan)
+        for agent in self.agents:
+            while len(agent.path)<makespan:
+                agent.path.append(agent.path[-1]) 
+
+    def generate_new_request(self):
+        for port in self.occupied_port:
+            eta=np.random.random()
+            if eta<self.retrieving_probability and len(self.parked_agents)!=0:
+                num_p=len(self.parked_agents)
+                k=np.random.randint(num_p)
+                ak=self.parked_agents[k]
+                ak.goal=port
+                self.retrieving_agents.append(ak)
+                self.parked_agents.pop(k)
+                self.occupied_port.add(port)
+                #retrieve a car
+            elif eta<self.retrieving_probability+self.parking_probability and len(self.unused_agents)!=0:
+                num_p=len(self.unused_agents)
+                k=np.random.randint(num_p)
+                ak=self.unused_agents[k]
+                assert(ak.loc==OUTVERTEX)
+                ak.loc=port
+                self.parking_agents.append(ak)
+                self.unused_agents.pop(k)
+                self.occupied_port.add(port)
+
+    def sequential_planning(self):
+        pass
+    
+    def count_num_robots_at_row(self,row):
+        num=0
+        for i in range(0,self.instance.xmax):
+            if (i,row) in self.location_id:
+                num=num+1
+        return num
+
+     # find an empty slot at the same cplumn
+    def find_empty_slot_at_column(self,current_pos):
+        xc,yc=current_pos
+        for y in range(self.instance.ymax-3,-1,-1):
+            if (xc,y) not in self.location_id:
+                return (xc,y)
+        return None
+
+    # find the an empty slot for parking
+    def find_empty_slot(self,current_pos):
+        xc,yc=current_pos
+        for y in range(self.instance.ymax-3,-1,-1):
+            if self.count_num_robots_at_row(y)>=self.instance.xmax-2:
+                continue
+            open=[(xc,y)]
+            visited=set()
+            while len(open)!=0:
+                curr=open.pop(0)
+                visited.add(curr)
+                if curr not in self.location_id:
+                    # self.used_empty_slots.add(curr)
+                    return curr
+                right=(curr[0]+1,curr[1])
+                if right[0]<self.instance.xmax-1 and right not in visited:
+                    open.append(right)
+                left=(curr[0]-1,curr[1])
+                if left[0]>=1 and left not in visited:
+                    open.append(left)
+        return None
+
+    #bring an escort to column 
+    def bring_escort_to_column(self,escort:Tuple[int,int],desired_column:int):
+        xe,ye=escort
+        
+        if xe==desired_column:
+            return
+        if xe<desired_column:
+            dx=1
+        elif xe>desired_column:
+            dx=-1
+        curr=xe
+        # curr=xe+dx
+        while curr!=desired_column:
+            #parking first
+            print((curr,ye),desired_column,"bring to escort")
+            # print((curr,xe))
+            if (curr+dx,ye) not in self.location_id:
+                print((curr+dx,ye), "not in dict")
+                return 
+            robot=self.location_id[(curr+dx,ye)]
+            # if robot in self.planned:
+            #     print(robot,"already planned line 162")
+            #     return 
+            next_v=(curr,ye)
+            # if self.clock+1 in self.v_table and next_v in self.v_table[self.clock+1]:
+            #     print("collisions",(curr+dx,ye),"to",next_v,"already occupied")
+            #     return
+            self.forward(robot,(curr+dx,ye),next_v)
+            curr=curr+dx
+            # print("curr=",curr)
+            # self.plans[robot]=[()]
+            # self.planned.add(robot)
+
+
+
+    def bring_escort_to_port(self,escort:Tuple[int,int],curr:Tuple[int,int]):
+        xe,ye=escort
+        yc=self.instance.ymax-3
+        assert(xe==curr[0])
+        ys=ye
+        while ys<yc:
+            ys=ys+1
+            # if (xe,ys) not in self.location_id:
+            #     print(escort,(xe,ys),(xe,ys) in self.used_empty_slots)
+            # assert((xe,ys) in self.location_id)
+            if (xe,ys) not in self.location_id:
+                return False
+            robot=self.location_id[(xe,ys)]
+            # if robot in self.planned:
+            #     return False
+            next_v=(xe,ys-1)
+            # if (self.clock+1) in self.v_table and next_v in self.v_table[self.clock+1]:
+            #     return False
+            self.forward(robot,(xe,ys),next_v)
+        return True
+
+    def ParkingMp(self,agent:Agent):
+        curr=agent.loc
+   
+        if curr in self.instance.all_slots:
+            return
+        escort=self.find_empty_slot_at_column(curr)
+        if escort is None:
+            escort=self.find_empty_slot(curr)
+            if escort is not None:
+                print("the escort X is", escort,"for agent ",agent.id,"at",curr)
+                self.bring_escort_to_column(escort,curr[0])
+                
+                self.clock=self.clock+1
+                self.fill_paths(self.clock)
+        escort=self.find_empty_slot_at_column(curr)
+        if escort is not None:
+            print("the escort is", escort,"for agent ",agent.id,"at",curr)
+            flag=self.bring_escort_to_port(escort,curr)
+            if flag==True:
+                x,y=curr
+                while y>self.instance.ymax-3:
+                    y=y-1
+                    self.clock=self.clock+1
+                    agent.path.append((x,y))
+                self.location_id.pop(curr)
+                agent.loc=agent.path[-1]
+                self.location_id[agent.loc]=agent.id
+                self.fill_paths(self.clock)
+                # self.plans[agent]=path[1:]
+                # self.reserve_path(path)
+            else:
+                return
+
+    def RetrieveMp(self,agent:int):
+        ai=self.agents[agent]
+        
+        reserved=set()
+        while ai.loc!=ai.goal:
+            
+            if self.clock>MAX_HORIZON:
+                break
+            self.moved.clear()
+            reserved.clear()
+            if ai.loc[1]<self.instance.ymax-2:
+                self.clock=self.clock+1
+                next_v=(ai.loc[0],ai.loc[1]+1)
+                next_next_v=(ai.loc[0],ai.loc[1]+2)
+                if next_v not in self.location_id:
+                    reserved.add(ai.loc)
+                    self.forward(ai.id,ai.loc,next_v)
+                    reserved.add(next_v)
+                else:
+                    aj=self.location_id[next_v]
+                    self.YieldMp(aj,reserved)
+                    self.moved[ai.id]=False
+                if next_next_v[1]<self.instance.ymax-1 and next_next_v in self.location_id:
+                    ajj=self.location_id[next_next_v]
+                    # print("yielding for ajj",ajj,next_next_v,self.agents[ajj].loc)
+                    self.YieldMp(ajj,reserved)
+                    
+                    self.moved[ai.id]=False
+                
+            else:
+                x,y=ai.loc
+                self.location_id.pop((x,y))
+                xg,yg=ai.goal
+                if x<xg:
+                    dx=1
+                else:
+                    dx=-1
+                while x!=xg:
+                    self.clock=self.clock+1
+                    x=x+dx
+                    ai.path.append((x,y))
+                self.clock=self.clock+1
+                ai.path.append((xg,yg))
+                ai.loc=ai.goal
+                
+                self.location_id[ai.loc]=ai.id
+            # for a in self.outOfBoundAgents.copy():
+            #     if a in self.moved:
+            #         continue
+            #     self.MoveBack(a,reserved)
+                # if self.agents[a].loc[0]>0 and self.agents[a].loc[0]<self.instance.xmax-1:
+                #     self.outOfBoundAgents.remove(a)
+                
+            self.fill_paths(self.clock)
+            print("timestep=",self.clock)
+
+      
+
+    def forward(self,agent:int,curr:Tuple[int,int],next:Tuple[int,int]):
+        self.agents[agent].loc=next
+        self.agents[agent].path.append(next)
+        self.location_id.pop(curr)
+        print(curr,"is popoed for agent",agent,"move to ",next)
+        self.location_id[next]=agent
+        self.moved[agent]=True
+        if next[0]==0 or next[0]==self.instance.xmax-1:
+            print("agent out of bounds, agent=",agent ,next)
+            self.outOfBoundAgents.add(agent)
+
+        
+        
+    
+    def YieldMp(self,agent:int,reserved:set,direction=None):
+        if agent in self.moved:
+            return self.moved[agent]
+        xa,ya=self.agents[agent].loc
+        # assert((xa,ya) in self.location_id)
+        if xa>self.instance.xmax/2:
+            first_direc="right"
+            fdx=1
+            second_direc="left"
+            sdx=-1
+        else:
+            first_direc="left"
+            second_direc="right"
+            fdx=-1
+            sdx=1
+        if (direction is None or direction==first_direc):
+            
+            next_v=(xa+fdx,ya)
+            if next_v not in self.location_id and (next_v[0]>=0 and next_v[0]<self.instance.xmax):
+                self.forward(agent,(xa,ya),next_v)
+                return True
+            elif next_v in self.location_id:
+                assert(next_v[0]>=0 and next_v[0]<self.instance.xmax)
+                aj=self.location_id[next_v]
+                if self.YieldMp(aj,reserved,first_direc)==True:
+                    self.forward(agent,(xa,ya),next_v)
+                    return True
+            # print("will this be called?")
+              
+        if (direction is None or direction==second_direc):
+            next_v=(xa+sdx,ya)
+            if next_v not in self.location_id and (next_v[0]>=0 and next_v[0]<self.instance.xmax):
+                self.forward(agent,(xa,ya),next_v)
+                return True
+            elif next_v in self.location_id:
+                aj=self.location_id[next_v]
+                if self.YieldMp(aj,reserved,second_direc)==True:
+                    self.forward(agent,(xa,ya),next_v)
+                    return True
+
+        self.moved[agent]=False
+        return False
+
+
+
+    def reserve_path(self):
+        pass
+    
+
+    def mcp_execute(self):
+        pass
+
+    def sim(self):
+        while True:
+            self.clock=self.clock+1
+            if self.clock>MAX_HORIZON:
+                break
+            self.main_loop()
+
+
+    def main_loop(self):
+        self.generate_new_request()
+        self.sequential_planning()
+        self.mcp_execute()
+        
+
+    def init_system(self):
+        #io ports
+        self.io_ports=[]
+        for x in range(1,self.xmax-1):
+            self.io_ports.append((x,self.ymax-1))
+    
+        #parking spots
+        self.parking_spots=[]
+        for x in range(1,self.xmax-1):
+            for y in range(0,self.ymax-2):
+               self.parking_spots.append((x,y))
+
+        # unused agents
+        self.unused_agents=[Agent(OUTVERTEX,None,i) for i in range(len(self.parking_spots))]
+        # vertices and visiting orders
+        self.vertices=[Vertex(x,y) for x in range(self.xmax) for y in range(self.ymax)]
+
+
+    def check_perpendicular(self,v1,v2,v3):
+        x1,y1=v1
+        x2,y2=v2
+        x3,y3=v3
+        if (x2-x1)*(y3-y2)-(y2-y1)*(x3-x2)!=0:
+            print("perpendicular following conflicts")
+            return True
+        return False
+
+    
+
+        
+
+         
